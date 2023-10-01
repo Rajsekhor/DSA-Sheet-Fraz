37abb17d397f5ed6bbf9d4c396a8dc20d3bd1bcc
Rajsekhor Saikia <raj.saikia.rick.333@gmail.com>
2023-10-01 11:39:56 +0530

    Find Pivot Index

---
+++ FInd-Pivot-Index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n=nums.size();
+        int rsum=accumulate(nums.begin(), nums.end(), 0);
+        int lsum=0;
+        for(int i=0;i<n;i++){
+            rsum-=nums[i];
+            if(lsum==rsum){
+                return i;
+            }
+            lsum+=nums[i];
+        }
+        return -1;
+    }
+};
